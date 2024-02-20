48f1f907aafadd379cf5bd2f628a3bd10da8482a
Gaurav-822 <gaurav.moocs@gmail.com>
2024-02-21 03:41:33 +0530

    image_integration

--- lib/chats/chat_row.dart
+++ lib/chats/chat_row.dart
@@ -1,4 +1,6 @@
 // for now i will make the chat row as a stateless widget!
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatRow extends StatelessWidget {
@@ -10,6 +12,27 @@ class ChatRow extends StatelessWidget {
     required this.profilePic,
     required this.onTap,
   });
+
+  Future<String?> getProfilePicUrl(String name) async {
+    try {
+      final querySnapshot = await FirebaseFirestore.instance
+          .collection('users')
+          .doc('profile')
+          .collection('admin')
+          .where('name', isEqualTo: name)
+          .get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        return querySnapshot.docs.first.data()['url'] as String?;
+      } else {
+        print('No profile pic found for $name');
+        return null;
+      }
+    } catch (error) {
+      print('Error retrieving profile pic: $error');
+      return null;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,18 +57,38 @@ class ChatRow extends StatelessWidget {
                 ),
               ),
               Container(
-                width: 50,
-                height: 50,
-                clipBehavior: Clip.antiAlias,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                ),
-                child: Image.asset(
-                  // profilePic,
-                  'assets/dummy_user.jpg',
-                  fit: BoxFit.cover,
-                ),
-              ),
+                  width: 50,
+                  height: 50,
+                  clipBehavior: Clip.antiAlias,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                  ),
+                  child: FutureBuilder<String?>(
+                    future: getProfilePicUrl(name),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<String?> snapshot) {
+                      if (snapshot.hasError) {
+                        return Text('Error: ${snapshot.error}');
+                      } else {
+                        // Data has been successfully retrieved
+                        String? profilePicPath = snapshot.data;
+                        return CachedNetworkImage(
+                          imageUrl: profilePicPath ??
+                              '', // Use an empty string for local assets
+                          imageBuilder: (context, imageProvider) => Image(
+                            image: imageProvider,
+                            fit: BoxFit.cover,
+                          ),
+                          placeholder: (context, url) =>
+                              CircularProgressIndicator(), // Placeholder widget while loading
+                          errorWidget: (context, url, error) => Image.asset(
+                            'assets/dummy_user.jpg',
+                            fit: BoxFit.cover,
+                          ),
+                        );
+                      }
+                    },
+                  )),
             ],
           ),
         ),
--- lib/pages/persona_page.dart
+++ lib/pages/persona_page.dart
@@ -1,4 +1,10 @@
+import 'dart:io';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -17,6 +23,10 @@ class _PersonaPageState extends State<PersonaPage> {
 
   late TextEditingController? _qrController = TextEditingController();
 
+  String? _imageUrl;
+  final _picker = ImagePicker();
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
   @override
   void initState() {
     super.initState();
@@ -24,6 +34,87 @@ class _PersonaPageState extends State<PersonaPage> {
     _adminController = TextEditingController();
     _loadProfileName();
     _loadQrCode();
+  }
+
+  void addProfilePic(String name, String url) {
+    FirebaseFirestore.instance
+        .collection('users')
+        .doc("profile")
+        .collection("admin")
+        .where('name', isEqualTo: name)
+        .get()
+        .then((QuerySnapshot querySnapshot) {
+      if (querySnapshot.docs.isNotEmpty) {
+        // If a document with the same name exists, update its URL
+        querySnapshot.docs.forEach((doc) {
+          doc.reference.update({'url': url}).then((_) {
+            print("Profile pic for $name updated");
+          }).catchError((error) {
+            print("Failed to update profile pic: $error");
+          });
+        });
+      } else {
+        // If no document with the same name exists, add a new document
+        FirebaseFirestore.instance
+            .collection('users')
+            .doc("profile")
+            .collection("admin")
+            .add({
+          'name': name,
+          'url': url,
+        }).then((DocumentReference docRef) {
+          print("Profile pic for $name added @: ${docRef.id}");
+        }).catchError((error) {
+          print("Failed to add profile pic: $error");
+        });
+      }
+    }).catchError((error) {
+      print("Error fetching document: $error");
+    });
+  }
+
+  Future<void> _pickAndUploadImage() async {
+    try {
+      final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+
+      if (pickedFile != null) {
+        final imageRef =
+            FirebaseStorage.instance.ref().child('images/$profile_name.jpg');
+        await imageRef.putFile(File(pickedFile.path));
+        _imageUrl = await imageRef.getDownloadURL();
+
+        setState(() {
+          addProfilePic(profile_name, _imageUrl!);
+        });
+      }
+    } on FirebaseException catch (e) {
+      // Handle Firebase-specific errors (e.g., storage full)
+      print('Firebase error: $e');
+    } catch (e) {
+      // Handle other exceptions (e.g., permissions)
+      print('Other error: $e');
+    }
+  }
+
+  Future<String?> getProfilePicUrl(String name) async {
+    try {
+      final querySnapshot = await FirebaseFirestore.instance
+          .collection('users')
+          .doc('profile')
+          .collection('admin')
+          .where('name', isEqualTo: name)
+          .get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        return querySnapshot.docs.first.data()['url'] as String?;
+      } else {
+        print('No profile pic found for $name');
+        return null;
+      }
+    } catch (error) {
+      print('Error retrieving profile pic: $error');
+      return null;
+    }
   }
 
   _loadProfileName() async {
@@ -36,7 +127,7 @@ class _PersonaPageState extends State<PersonaPage> {
       });
     } else {
       setState(() {
-        profile_name = 'Hello, $admin!';
+        profile_name = admin;
       });
     }
   }
@@ -60,7 +151,7 @@ class _PersonaPageState extends State<PersonaPage> {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.setString("admin", adminName);
     setState(() {
-      profile_name = 'Hello, $adminName!';
+      profile_name = adminName;
     });
   }
 
@@ -92,7 +183,7 @@ class _PersonaPageState extends State<PersonaPage> {
                     ).createShader(bounds);
                   },
                   child: Text(
-                    profile_name,
+                    "Hello, $profile_name!",
                     style: TextStyle(
                       fontFamily: 'amatic',
                       fontSize: 48,
@@ -103,24 +194,46 @@ class _PersonaPageState extends State<PersonaPage> {
                 ),
               ),
             ),
-            Align(
-              alignment: Alignment.center,
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(8, 16, 8, 0),
-                child: Container(
-                  width: 120,
-                  height: 120,
-                  clipBehavior: Clip.antiAlias,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                  ),
-                  child: Image.asset(
-                    'assets/dummy_user.jpg',
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-            ),
+            FutureBuilder<String?>(
+                future: getProfilePicUrl(profile_name),
+                builder:
+                    (BuildContext context, AsyncSnapshot<String?> snapshot) {
+                  if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else {
+                    // Data has been successfully retrieved
+                    String? imageUrl = snapshot.data;
+                    return Align(
+                      alignment: Alignment.center,
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(8, 16, 8, 0),
+                        child: Container(
+                          width: 120,
+                          height: 120,
+                          clipBehavior: Clip.antiAlias,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                          ),
+                          child: GestureDetector(
+                            onTap: () {
+                              _pickAndUploadImage();
+                            },
+                            child: CachedNetworkImage(
+                              imageUrl: imageUrl ?? '',
+                              fit: BoxFit.cover,
+                              placeholder: (context, url) =>
+                                  CircularProgressIndicator(), // Placeholder widget while loading
+                              errorWidget: (context, url, error) => Image.asset(
+                                "assets/dummy_user.jpg",
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  }
+                }),
             Row(
               mainAxisSize: MainAxisSize.max,
               children: [
