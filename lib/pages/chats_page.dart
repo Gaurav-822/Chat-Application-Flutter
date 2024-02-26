d8774ee2b0042ba07feab615c9ec4b9a88d6cfe1
Gaurav-822 <gaurav.moocs@gmail.com>
2024-02-26 12:14:26 +0530

    profile_pic size fixed

--- lib/pages/chats_page.dart
+++ lib/pages/chats_page.dart
@@ -1,40 +1,62 @@
 import 'package:chat_app/chats/chat_row.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Chats extends StatefulWidget {
   const Chats({super.key});
 
   @override
-  State<StatefulWidget> createState() => _Chats();
+  State<StatefulWidget> createState() => _ChatsState();
 }
 
-class _Chats extends State<Chats> {
+class _ChatsState extends State<Chats> {
+  List<String> chatNames = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadChatNames();
+    _saveChatName("Gaurav");
+    _saveChatName("Padmaja");
+    _saveChatName("Papu");
+    _saveChatName("Mummy");
+  }
+
+  Future<void> _loadChatNames() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> storedChatNames = prefs.getStringList('chatNames') ?? [];
+    setState(() {
+      chatNames = storedChatNames;
+    });
+  }
+
+  Future<void> _saveChatName(String name) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (!chatNames.contains(name)) {
+      // Check if the name is not already in the list
+      chatNames.add(name); // Add the new name to the list
+      await prefs.setStringList('chatNames',
+          chatNames); // Save the updated list to shared preferences
+      setState(() {}); // Trigger a rebuild to reflect the changes
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
+    return ListView.builder(
       padding: EdgeInsets.zero,
       scrollDirection: Axis.vertical,
-      children: [
-        ChatRow(
-          name: "Gaurav",
+      itemCount: chatNames.length,
+      itemBuilder: (BuildContext context, int index) {
+        final String name = chatNames[index];
+        return ChatRow(
+          name: name,
           onTap: () {
-            GoRouter.of(context).go('/chats/Gaurav');
+            GoRouter.of(context).go('/chats/$name');
           },
-        ),
-        ChatRow(
-          name: "Padmaja",
-          onTap: () {
-            GoRouter.of(context).go('/chats/Padmaja');
-          },
-        ),
-        ChatRow(
-          name: "Gugu",
-          onTap: () {
-            GoRouter.of(context).go('/chats/Gugu');
-          },
-        ),
-      ],
+        );
+      },
     );
   }
 }
