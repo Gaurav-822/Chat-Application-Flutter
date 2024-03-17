7d0cbd0b0e75fe2b2d87d6da87b5566231cfd156
Gaurav-822 <gaurav.moocs@gmail.com>
2024-03-17 21:27:50 +0530

    loading screen added

--- lib/signIn/login.dart
+++ lib/signIn/login.dart
@@ -1,7 +1,9 @@
+import 'package:chat_app/Functions/toasts.dart';
 import 'package:chat_app/signIn/auth/email_pass_auth.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:lottie/lottie.dart';
 
 class LogIn extends StatefulWidget {
   const LogIn({super.key});
@@ -16,199 +18,236 @@ class _LogIn extends State<LogIn> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
+  bool isLoading = false;
+
+  Future<void> signInTool() async {
+    setState(() {
+      isLoading = true;
+    });
+    try {
+      signUserIn(emailController.text, passwordController.text);
+      await Future.delayed(const Duration(seconds: 2));
+    } catch (error) {
+      showToastMessage('Error signing in: $error');
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         top: true,
         child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(8),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Padding(
-                  padding: const EdgeInsetsDirectional.fromSTEB(0, 16, 0, 8),
-                  child: Text(
-                    'Log In',
-                    style: Theme.of(context).textTheme.displayLarge?.copyWith(
-                          // color: Colors.white, // Text color can be adjusted
-                          fontWeight: FontWeight
-                              .bold, // Example: Applying bold font weight
-                        ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(24),
-                    child: Image.asset(
-                      'assets/neon_butterfly.png',
-                      width: 250,
-                      height: 250,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(4),
-                  child: TextFormField(
-                    controller: emailController,
-                    autofocus: false,
-                    obscureText: false,
-                    decoration: const InputDecoration(
-                      labelText: 'Email',
-                      border: InputBorder.none,
-                    ),
-                    style: Theme.of(context).textTheme.bodyLarge,
-                    validator: (value) {
-                      // Add your validation logic here
-                      return null;
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(4),
-                  child: TextFormField(
-                    controller: passwordController,
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      labelText: 'Password',
-                      border: InputBorder.none,
-                    ),
-                    style: Theme.of(context).textTheme.bodyLarge,
-                    validator: (value) {
-                      // Add your validation logic here
-                      return null;
-                    },
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        signUserIn(
-                            emailController.text, passwordController.text);
-                      },
-                      style: ElevatedButton.styleFrom(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 24, vertical: 0),
-                        backgroundColor: Theme.of(context).primaryColor,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(24),
-                        ),
-                        elevation: 3,
-                        side: const BorderSide(
-                          color: Colors.transparent,
-                          width: 1,
-                        ),
-                      ),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Padding(
+                      padding:
+                          const EdgeInsetsDirectional.fromSTEB(0, 16, 0, 8),
                       child: Text(
-                        'Login',
-                        style: TextStyle(
-                          fontFamily: 'Readex Pro',
-                          fontSize:
-                              Theme.of(context).textTheme.titleMedium!.fontSize,
-                          color: Colors.white,
+                        'Log In',
+                        style:
+                            Theme.of(context).textTheme.displayLarge?.copyWith(
+                                  // color: Colors.white, // Text color can be adjusted
+                                  fontWeight: FontWeight
+                                      .bold, // Example: Applying bold font weight
+                                ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(24),
+                        child: Image.asset(
+                          'assets/neon_butterfly.png',
+                          width: 250,
+                          height: 250,
+                          fit: BoxFit.cover,
                         ),
                       ),
                     ),
-                  ),
-                ),
-                Align(
-                  alignment: const AlignmentDirectional(1, 0),
-                  child: Padding(
-                    padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 8, 0),
-                    child: GestureDetector(
-                      onTap: () {
-                        resetPassword(emailController.text);
-                      },
-                      child: Text(
-                        'Forget Password?',
+                    Padding(
+                      padding: const EdgeInsets.all(4),
+                      child: TextFormField(
+                        controller: emailController,
+                        autofocus: false,
+                        obscureText: false,
+                        decoration: const InputDecoration(
+                          labelText: 'Email',
+                          border: InputBorder.none,
+                        ),
                         style: Theme.of(context).textTheme.bodyLarge,
+                        validator: (value) {
+                          // Add your validation logic here
+                          return null;
+                        },
                       ),
                     ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsetsDirectional.fromSTEB(0, 8, 0, 0),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(16),
-                        child: Text(
-                          'OR',
-                          style: Theme.of(context).textTheme.bodyLarge,
+                    Padding(
+                      padding: const EdgeInsets.all(4),
+                      child: TextFormField(
+                        controller: passwordController,
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          labelText: 'Password',
+                          border: InputBorder.none,
                         ),
+                        style: Theme.of(context).textTheme.bodyLarge,
+                        validator: (value) {
+                          // Add your validation logic here
+                          return null;
+                        },
                       ),
-                      const SizedBox(
-                        height: 100,
-                        child: VerticalDivider(
-                          thickness: 1,
-                          indent: 16,
-                          endIndent: 16,
-                        ),
-                      ),
-                      Expanded(
-                        child: Container(
-                          width: 100,
-                          height: 100,
-                          decoration: const BoxDecoration(
-                            shape: BoxShape.rectangle,
-                          ),
-                          child: const Row(
-                            mainAxisSize: MainAxisSize.max,
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Icon(
-                                Icons.android,
-                                size: 24,
-                              ),
-                              Icon(
-                                Icons.facebook,
-                                size: 24,
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                    padding: const EdgeInsetsDirectional.fromSTEB(0, 8, 0, 0),
-                    child: RichText(
-                      text: TextSpan(
-                        children: [
-                          const TextSpan(
-                            text: 'New User - ',
-                            style: TextStyle(
-                              fontFamily: 'Readex Pro',
-                              fontWeight: FontWeight.bold,
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            signInTool();
+                          },
+                          style: ElevatedButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 24, vertical: 0),
+                            backgroundColor: Theme.of(context).primaryColor,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(24),
+                            ),
+                            elevation: 3,
+                            side: const BorderSide(
+                              color: Colors.transparent,
+                              width: 1,
                             ),
                           ),
-                          TextSpan(
-                            text: 'Sign In',
-                            recognizer: TapGestureRecognizer()
-                              ..onTap = () {
-                                // Add your sign-in logic here
-                                GoRouter.of(context).go('/signin');
-                              },
-                            style: const TextStyle(
-                              color: Colors
-                                  .blue, // Change color to indicate it's clickable
+                          child: Text(
+                            'Login',
+                            style: TextStyle(
+                              fontFamily: 'Readex Pro',
+                              fontSize: Theme.of(context)
+                                  .textTheme
+                                  .titleMedium!
+                                  .fontSize,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: const AlignmentDirectional(1, 0),
+                      child: Padding(
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(0, 0, 8, 0),
+                        child: GestureDetector(
+                          onTap: () {
+                            resetPassword(emailController.text);
+                          },
+                          child: Text(
+                            'Forget Password?',
+                            style: Theme.of(context).textTheme.bodyLarge,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsetsDirectional.fromSTEB(0, 8, 0, 0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(16),
+                            child: Text(
+                              'OR',
+                              style: Theme.of(context).textTheme.bodyLarge,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 100,
+                            child: VerticalDivider(
+                              thickness: 1,
+                              indent: 16,
+                              endIndent: 16,
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              width: 100,
+                              height: 100,
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.rectangle,
+                              ),
+                              child: const Row(
+                                mainAxisSize: MainAxisSize.max,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Icon(
+                                    Icons.android,
+                                    size: 24,
+                                  ),
+                                  Icon(
+                                    Icons.facebook,
+                                    size: 24,
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
                         ],
-                        style: Theme.of(context).textTheme.bodyLarge,
                       ),
-                    )),
-              ],
-            ),
+                    ),
+                    Padding(
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(0, 8, 0, 0),
+                        child: RichText(
+                          text: TextSpan(
+                            children: [
+                              const TextSpan(
+                                text: 'New User - ',
+                                style: TextStyle(
+                                  fontFamily: 'Readex Pro',
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              TextSpan(
+                                text: 'Sign In',
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () {
+                                    // Add your sign-in logic here
+                                    GoRouter.of(context).go('/signin');
+                                  },
+                                style: const TextStyle(
+                                  color: Colors
+                                      .blue, // Change color to indicate it's clickable
+                                ),
+                              ),
+                            ],
+                            style: Theme.of(context).textTheme.bodyLarge,
+                          ),
+                        )),
+                  ],
+                ),
+              ),
+              if (isLoading)
+                Center(
+                  child: Lottie.asset(
+                    "assets/lotties/loading.json",
+                    repeat: true,
+                    frameRate: const FrameRate(144),
+                  ),
+                ),
+            ],
           ),
         ),
       ),
